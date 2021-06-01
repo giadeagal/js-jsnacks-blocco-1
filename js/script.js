b5541dddb50121c266b5e4a35f43bb7bc72c2188
Gianluca <gl.galassini@gmail.com>
2021-06-01 16:02:05 +0200

    snack 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-document.getElementById("printer").innerHTML = "Hello World!" 
+// Snack 1
+document.getElementById("title").innerHTML = "Qual è la parola più corta?"
+
+var wordOne = prompt("Inserisci una parola");
+var wordTwo = prompt("Inserisci una nuova parola");
+
+document.getElementById("printer").innerHTML = wordOne + " e " + wordTwo + " hanno lo stesso numero di caratteri"
+
+if (wordOne.length < wordTwo.length) {
+    document.getElementById("printer").innerHTML = wordOne + " è una parola più corta di " + wordTwo
+} else if (wordOne.length > wordTwo.length) {
+    document.getElementById("printer").innerHTML = wordTwo + " è una parola più corta di " + wordOne
+};
